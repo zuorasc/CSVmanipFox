0bd473827f716209185bc39a4188bfb6bdf4d6d6
Mike Sullivan <mike.sullivan@zuora.com>
2012-10-05 13:25:24 -0700

    hotfix for delta file

--- src/com/zuora/msullivan/CSVmanip.java
+++ src/com/zuora/msullivan/CSVmanip.java
@@ -29,6 +29,7 @@ public class CSVmanip {
 	
 	private static CsvWriter outputWriter;
 	static private String outputFile;
+	static private File logFile;
 	
 	private static int lookupColumn;
 	private static int inputColumn;
@@ -43,21 +44,26 @@ public class CSVmanip {
 	private static boolean replaceValue = false;
 	private static int replaceColumn;
 	
-	private static HashMap<String, ArrayList<String>> lookupValueHash;
+	private static HashMap<String, String> lookupValueHash;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Add logFile writer
 		
 		if(args.length < 3 || args.length > 4){
 			System.out.println("Usage: <options> <CSV look up file> <CSV input file> <output file>");
 			return;
 		}
 		
-		lookupValueHash = new HashMap<String, ArrayList<String>>();
+		logFile = new File("log.out");
 		
-		if(args[0].charAt(0) == '-'){ parseOptions(args[0].charAt(1));}
+		lookupValueHash = new HashMap<String, String>();
+		
+		if(args[0].charAt(0) == '-'){ 
+			parseOptions(args[0].charAt(1));
+			hasOptions = true;
+		}
 		
 		if(!hasOptions) {
 			lookupFile = args[0];
@@ -65,13 +71,13 @@ public class CSVmanip {
 		} else { 
 			lookupFile = args[1];
 			inputFile = args[2];
-			}
+		}
 		
 		getLookup();
 		getInput();
 		doLookUp();
 		
-		if(replaceValue) {
+		if(hasOptions) {
 			outputFile = args[1].toString() + ".out";
 		} else { outputFile = args[3]; }
 		writeOutput();
@@ -88,8 +94,8 @@ public class CSVmanip {
 		case 'v':   variableOut = hasOptions = true;
 					lookupOutputColumns = new ArrayList<String>(5);
 					break;
-		case 'r':   replaceValue = hasOptions = true;
-					lookupOutputColumns = new ArrayList<String>(5);
+		case 'r':   replaceValue = true;
+					//lookupOutputColumns = new ArrayList<String>(5);
 					break;
 		
 		}
@@ -113,15 +119,15 @@ public class CSVmanip {
 			} else { System.out.println("Error Reading lookup headers"); return;}
 			
 			System.out.println("Enter Column number to use for lookup");
-			lookupColumn = 4;
-			System.out.println("DEBUG "+lookupColumn);
-			//lookupColumn = in.nextInt();
+//			lookupColumn = 4;
+//			System.out.println("DEBUG "+lookupColumn);
+			lookupColumn = in.nextInt();
 			
 			if(replaceValue){
 				System.out.println("Enter Column to replace");
-				replaceColumn = lookupColumn;
-				System.out.println("DEBUG " + replaceColumn);
-//				replaceColumn = in.nextInt();
+//				replaceColumn = lookupColumn;
+//				System.out.println("DEBUG " + replaceColumn);
+				replaceColumn = in.nextInt();
 			}
 			
 			
@@ -157,14 +163,14 @@ public class CSVmanip {
 			} else { System.out.println("Error Reading input headers"); return;}
 			
 			System.out.println("Enter Column number to use for input");
-			inputColumn = 0;
-			System.out.println("DEBUG " + inputColumn);
-//			inputColumn = in.nextInt();
+//			inputColumn = 1;
+//			System.out.println("DEBUG " + inputColumn);
+			inputColumn = in.nextInt();
 			
 			System.out.println("Enter Column number to use for value");
-			valueColumn = 1;
-			System.out.println("DEBUG " + valueColumn);
-//			valueColumn = in.nextInt();
+//			valueColumn = 14;
+//			System.out.println("DEBUG " + valueColumn);
+			valueColumn = in.nextInt();
 			
 			if(variableOut){
 				System.out.println("Enter Column numbers to print, in order");
@@ -194,21 +200,28 @@ public class CSVmanip {
 	static void doLookUp(){
 		
 		String key;
-		
+		ArrayList<String> keys = new ArrayList<String>();
 		try {
 			// read the lookup file pull out keys put in hashmap
 			while(lookupReader.readRecord()){
 				key = lookupReader.get(lookupColumn);
-				lookupValueHash.put(key, new ArrayList<String>(4));
+				if (key.length() > 0 && !key.isEmpty()) {
+					//if key is not null or empty
+					keys.add(key);
+				}
 			}
-			Set<String> keys = lookupValueHash.keySet();
+			//Set<String> keys = lookupValueHash.keySet();
 			String value;
 			//read input file if key is in hashmap add to arraylist
 			while(inputReader.readRecord()){
 				key = inputReader.get(inputColumn);
 				if(keys.contains(key)){
 					value = inputReader.get(valueColumn);
-					lookupValueHash.get(key).add(value);
+					if(value.length() > 0 && !value.isEmpty()) {
+						//if the value is not null or empty	
+						lookupValueHash.put(key, value);
+					}
+					
 				}
 			}
 		} catch (IOException e) {
@@ -233,40 +246,83 @@ public class CSVmanip {
 				lookupReader.close();
 				lookupReader = new CsvReader(lookupFile);
 				lookupReader.readHeaders();
+				//TODO: should only print matches
+				Integer line = 0;
 				while(lookupReader.readRecord()){
+					
 					//go through the headers and check to see if it is the one to replace
 					//print value accordingly
 					for(String header: lookupReader.getHeaders()){
-						if(header.equals(lookupReader.getHeader(3))){
-							String ccNum = lookupValueHash.get(lookupReader.get(3)).get(0);
-							String ccTypeCode =  ccNum.substring(0, 0);
-							int ccTypeCodeInt = Integer.parseInt(ccTypeCode);
-							switch (ccTypeCodeInt) {
-							case 4 : 
-								outputWriter.write("Visa");
-								break;
-							case 5 : 
-								outputWriter.write("MasterCard");
-								break;
-							case 6 :
-								outputWriter.write("Discover");
-								break;
-							case 3 :
-								outputWriter.write("AmericanExpress");
-								break;
-							default :
-								outputWriter.write("UNKNOWN");
-							
+						if (lookupReader.get(replaceColumn) == null ) {
+							System.out.println("null PTProfile for Account: " + lookupReader.get(1) + "@ input line " + line);
+							outputWriter.write(""); 
+						} 
+						else if (lookupValueHash.get(lookupReader.get(replaceColumn)) == null) {
+							System.out.println("null card for PTProfile:" + lookupReader.get(replaceColumn) + "@ input line " + line);
+							outputWriter.write("");
+						}
+						else if(header.equals(lookupReader.getHeader(2))){
+							outputWriter.write("CreditCard");
+						}
+						else if(header.equals(lookupReader.getHeader(3))){
+							//if(lookupValueHash.get(lookupReader.get(replaceColumn)).get(0) != null) {
+								//String ccNum = lookupValueHash.get(lookupReader.get(replaceColumn)).get(0);
+								try { 
+									if (lookupReader.get(replaceColumn) == null ) {
+										System.out.println("null PTProfile for Account @input line " + line);
+										outputWriter.write(""); 
+									}
+									String ccNum = lookupValueHash.get(lookupReader.get(replaceColumn));
+									if(ccNum == null ) { 
+										System.out.println("null card for PTProfileId @input line " + line);
+										//outputWriter.write("");
+										ccNum = "100";
+									}
+									String ccTypeCode =  ccNum.substring(0, 1);
+									int ccTypeCodeInt = Integer.parseInt(ccTypeCode);
+									
+									switch (ccTypeCodeInt) {
+									case 4 : 
+										outputWriter.write("Visa");
+										break;
+									case 5 : 
+										outputWriter.write("MasterCard");
+										break;
+									case 6 :
+										outputWriter.write("Discover");
+										break;
+									case 3 :
+										outputWriter.write("AmericanExpress");
+										break;
+									default :
+										outputWriter.write("UNKNOWN");
+									
+									}
+							} catch (Exception e) {
+								System.out.println("Exception in Card Type Lookup for PTProfileID @ input line " + line);
 							}
 						}
-						if(header.equals(lookupReader.getHeader(replaceColumn))){
-							outputWriter.write(lookupValueHash.get(lookupReader.get(replaceColumn)).get(0));
+						else if(header.equals(lookupReader.getHeader(replaceColumn))){
+							//replace field
+							try { 
+								if (lookupReader.get(replaceColumn) == null ) {
+									System.out.println("null PTProfile for Account @line " + line);
+									//break; 
+								}
+								String out = lookupValueHash.get(lookupReader.get(replaceColumn));
+							outputWriter.write(out);
+							} catch (Exception e) {
+								System.out.println("Exception in Card Type Lookup for PTProfileID @line " + line);
+								outputWriter.write("");
+							}
 						}
 						else {
+							//else copy to output
 							outputWriter.write(lookupReader.get(header));
 						}
 					}
 					outputWriter.endRecord();
+					line++;
 				}
 				outputWriter.close();
 			} catch (IOException e) {
@@ -287,11 +343,11 @@ public class CSVmanip {
 		
 			for(String s : lookupValueHash.keySet()){
 				outputWriter.write(s);
-				values = "";
-				for(String r : lookupValueHash.get(s)){
-					values = values + r + " ";
-				}
-				outputWriter.write(values);
+//				values = "";
+//				for(String r : lookupValueHash.get(s)){
+//					values = values + r + " ";
+//				}
+				outputWriter.write(lookupValueHash.get(s));
 				outputWriter.endRecord();
 			}
 			
